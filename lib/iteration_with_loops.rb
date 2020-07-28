3225124e7c7a463804cbf91d5af5c8ce55ef9034
Jake Peterson <jpeterson0514@gmail.com>
2020-07-28 02:54:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def join_nested_strings(src)
       if src[row_index][element_index] == src[row_index][element_index].to_s 
       empty = src[row_index][element_index]
   end
-  new_array << empty.uniq
+  new_array << empty
       element_index += 1 
     end 
     row_index += 1 
